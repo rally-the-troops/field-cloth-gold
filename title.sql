ded5554c5becaf25cc952e3be6c2e4b995729317
Tor Andersson <tor@ccxvii.net>
2023-07-16 01:05:07 +0200

    Without the the.

--- title.sql
+++ title.sql
@@ -1,1 +1,1 @@
-insert or replace into titles ( title_id, title_name, bgg ) values ( 'field-cloth-gold', 'The Field of the Cloth of Gold', 309752 );
+insert or replace into titles ( title_id, title_name, bgg ) values ( 'field-cloth-gold', 'Field of the Cloth of Gold', 309752 );
