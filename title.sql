53bfb5e35be76a46645db2115d227c5555ce6832
Tor Andersson <tor@ccxvii.net>
2023-09-21 21:05:31 +0200

    Add match making setups.

--- title.sql
+++ title.sql
@@ -1,1 +1,4 @@
-insert or replace into titles ( title_id, title_name, bgg ) values ( 'field-cloth-gold', 'Field of the Cloth of Gold', 309752 );
+insert or replace into titles ( title_id, title_name, bgg, is_symmetric ) values ( 'field-cloth-gold', 'Field of the Cloth of Gold', 309752, 1 );
+insert or ignore into setups ( title_id, player_count, scenario, options ) values
+	( 'field-cloth-gold', 2, 'Standard', '{}' )
+;
